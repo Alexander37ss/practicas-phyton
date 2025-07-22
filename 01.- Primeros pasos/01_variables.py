a72b8e17e02c231e9a567601732981f17bc20f83
Alexander37ss <alexanderpalazuelos37@gmail.com>
2025-07-21 17:06:13 -0700

    variables

---
+++ 01.- Primeros pasos/01_variables.py
@@ -0,0 +1,24 @@
+#Inicializacion de variables (Persona hipotetica)
+nombre = 'Luis'
+sexo = 'masculino'
+edad = 14
+nacionalidad = 'Mexicano'
+
+#Imprimir datos de la persona
+print(nombre)
+print('-----')
+print("Edad: ", edad)
+print("Sexo: ", sexo)
+print("Nacionalidad: ", nacionalidad, "\n\n")
+
+#Imprimir tipo de datos de variables
+print(type(1))
+print(type(1.1))
+print(type('cadena'))
+print(type(True),"\n\n")
+
+#Leer datos
+valoracion = input("Danos tu opinon sobre nuestro programa =)\n")
+print("Ultima opinion de un usuario random: ",valoracion)
+
+
