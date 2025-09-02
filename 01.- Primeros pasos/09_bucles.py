bfa6dd51d20e1553a121217cb65c622f26ff4d99
Alexander37ss <alexanderpalazuelos37@gmail.com>
2025-09-01 20:12:12 -0700

    bucles

---
+++ 01.- Primeros pasos/09_bucles.py
@@ -0,0 +1,15 @@
+#Bucles o Ciclos
+
+#While(Mientras)
+edad = 0
+
+while edad < 20: #Fabrica de cumpleaños
+    edad = edad + 1
+    print("Feliz cumpleaños!")
+print("Tienes", edad, "años")
+
+#For(Para)
+Lista = ["Nueva York", "Moscú", "Seoul", "Buenos Aires", "Frankfurt", "Ankara", "El Cairo", "Madrid", "Sidney", "Budapest"]
+
+for x in Lista:
+    print(x)
